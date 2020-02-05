067b7a2a373b7652f6a60e9243f4bd8586cd1d7d
CaityHeath <caity.heath@gmail.com>
2020-02-05 15:20:13 -0800

    initial scaffolding

---
+++ components/landing.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function Landing(){
+return(
+  <>
+    <h1>Hours</h1>
+      <ul>
+        <li>Monday - Saturday</li>
+        <li>11:00am - 7:00pm</li>
+      </ul> 
+
+      <ul>
+        <li>Sunday</li>
+        <li>11:00am - 6pm</li>
+      </ul> 
+    </>
+);
+}
+
+export default Landing;
---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Landing from "../components/landing";
+
+
+function Index() {
+  return(
+    <div>
+      <Landing/>
+    </div>
+  );
+}
+
+export default Index;
