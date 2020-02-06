43443b9094690184187f7a07d02dd14c665b5690
CaityHeath <caity.heath@gmail.com>
2020-02-05 17:00:25 -0800

    global styled components added

--- components/landing.js
+++ components/landing.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Title = styled.h2`
+  color: #D8E9C2;
+  font-size: 2em;
+`
 
 function Landing(){
 return(
   <>
-    <h2>Hours</h2>
+    <Title>
+      <h2>Hours</h2>
+     </Title> 
       <ul>
         <li>Monday - Saturday</li>
         <li>11:00am - 7:00pm</li>
--- pages/index.js
+++ pages/index.js
@@ -4,16 +4,27 @@ import Sell from '../components/sell';
 import Mission from '../components/mission';
 import Location from '../components/location';
 import ComingSoon from '../components/comingsoon';
+import styled, { ThemeProvider, createGlobalStyle } from 'styled-components';
+
+import Reset from '../src/style/ResetCSS';
+import Theme from '../src/style/theme';
+
+export const GlobalStyle = createGlobalStyle`
+    ${Reset}
+`;
 
 function Index() {
   return(
-    <div>
-      <Landing/>
-      <Sell/>
-      <Mission/>
-      <Location/>
-      <ComingSoon/>
-    </div>
+    <>
+      <ThemeProvider theme={Theme}>
+        <GlobalStyle />
+          <Landing/>
+          <Sell/>
+          <Mission/>
+          <Location/>
+          <ComingSoon/> 
+      </ThemeProvider>
+    </>
   );
 }
 
